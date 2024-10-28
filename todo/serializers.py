5cfd06255b59c7966ac71f7e345a213c7971e7cc
CronosCoder <mfoysal314@gmail.com>
2024-10-29 00:35:49 +0600

    user app added and priority added in Todo model

--- todo/serializers.py
+++ todo/serializers.py
@@ -5,13 +5,13 @@ from todo.models import Todo
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ["id","name", "description", "deadline", "is_active"]
+        fields = ["id","name", "description", "deadline", "is_active", "priority"]
 
 
 class TodoListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ["id","name", "deadline", "is_active"]
+        fields = ["id","name", "deadline", "is_active", "priority"]
 
 
 class TodoUpdateStatusSerializer(serializers.ModelSerializer):
