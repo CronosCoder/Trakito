4769c4d7db492ffbd4c8d79267c1734481182017
CronosCoder <mfoysal314@gmail.com>
2024-10-29 23:15:23 +0600

    user model added

--- todo/models.py
+++ todo/models.py
@@ -9,6 +9,10 @@ class Todo(BaseModel):
     deadline = models.DateTimeField()
     priority = models.CharField("Priority",max_length=20,choices=Priority.choices, default=Priority.LOW)
     finish_time = models.DateTimeField(null=True,blank=True)
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE, related_name="todo", null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.user} - {self.name}"
 
     class Meta:
         db_table = "todo_Todo"
--- user/models.py
+++ user/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from core.models import BaseModel
+from user.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
+    first_name = models.CharField(_('first name'), max_length=150)
+    last_name = models.CharField(_('last name'), max_length=150)
+    email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    @property
+    def full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
