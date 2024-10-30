d63571a9e7344dbe04a71b3856606ceaa4b8cd0f
CronosCoder <mfoysal314@gmail.com>
2024-10-30 23:51:27 +0600

    pagination and djoser modified

--- trakito/urls.py
+++ trakito/urls.py
@@ -5,9 +5,9 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
-    path('auth/jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
+    path('api/v1/auth/jwt/create/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/auth/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("api/v1/", include("todo.urls"))
 ]
